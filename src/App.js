5f33256660978ad52a13b78d127db724cf6a85f0
Neeraj <salunkeneeraj@gmail.com>
2021-07-25 21:04:06 +0530

    1st commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,103 @@
-import logo from './logo.svg';
-import './App.css';
+import React , {useEffect,useState} from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Electionabi from './contracts/Election.json';
+import Web3 from 'web3';
+import Navbar from './Navbar';
+import Body from './Body';
+import "./App.css";
 
 function App() {
+
+  useEffect(() => {
+    loadWeb3();
+    loadBlockchainData();
+  },[])
+
+  const[currentAccount,setCurrentAccount] = useState("");
+  const[loader,setLoader] = useState(true);
+  const[electionSM,setElectionSM] = useState();   /* SM means smart contract */
+  const[candidate1,setCandidate1] = useState();
+  const[candidate2,setCandidate2] = useState();
+
+  // loadweb3 is a part of boilerplate code.
+  const loadWeb3 = async() => {
+    if(window.ethereum)
+    {
+      window.web3 = new  Web3(window.ethereum);
+      await window.ethereum.enable();
+    }
+    else if(window.web3)
+    {
+      window.web3 = new Web3(window.web3.currentProvider);
+    }
+    else
+    {
+      window.alert("Metamask not detected");
+    }
+  };
+
+  // loadblockchaindata is a boiler plate code
+  const loadBlockchainData = async ()=>{
+    setLoader(true);
+    const web3 = window.web3;
+
+    const accounts=await web3.eth.getAccounts();
+    console.log("accounts:-"+accounts);
+    // const account = accounts[0];
+    setCurrentAccount(accounts[0]);
+
+    const networkId = await web3.eth.net.getId();
+
+    console.log("networkId:-"+networkId);
+
+    const networkData = Electionabi.networks[networkId];/* networkData is returning undefined */
+
+    console.log("networkData:-"+networkData);
+
+    var address="0x07F5c9F86b644CEc6EEe429189e21cf3924F268F";
+    if(networkId == 5777)
+    {
+      const election = new web3.eth.Contract(Electionabi.abi,address); //here we are calling our contract 
+      /* in brackets earlier was (Electionabi.abi,networkData.address) */
+      
+      const candidate1 = await election.methods.candidates(1).call(); //here we are calling candidates mapping from our smart contract
+      // const candidate1Id = candidate1.id;
+      // const candidate1Name = candidate1.name;
+      // const candidate1VoteCount = candidate1.votecount;
+
+      const candidate2 = await election.methods.candidates(2).call();
+      // const candidate2Id = candidate2.id;
+      // const candidate2Name = candidate2.name;
+      // const candidate2VoteCount = candidate2.votecount;
+      
+      setCandidate1(candidate1);
+      setCandidate2(candidate2);
+      setElectionSM(election);
+      setLoader(false);
+    }
+    else
+    {
+      window.alert("Smart contract is not deployed");
+    }
+  }
+
+  const votecandidate = async (candidateId) => {
+    setLoader(true);
+    await electionSM.methods.Vote(candidateId).send({from : currentAccount}).on("transactionhash",()=>{
+      console.log("Successfully Ran");
+    });
+    setLoader(false);
+  };
+
+  if(loader)
+  {
+    return <div>loading...</div>
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar account={currentAccount}/>  {/* This is navbar from navbar.js. We are passing account from here to Navbar.js */}
+      <Body candidate1={candidate1} candidate2={candidate2} votecandidate={votecandidate} account={currentAccount}/>
     </div>
   );
 }
---
+++ src/Body.js
@@ -0,0 +1,74 @@
+import React , {useState} from "react";
+import './App.css';
+
+
+
+const Body = ({candidate1,candidate2,votecandidate,account}) => {
+    const[candidate,setCandidate] = useState("");
+
+    // var can=0;
+    const[can,setCan] = useState();
+
+    // console.log("candidate :"+candidate);
+    //here we define a function onchange
+    const onchange = (e) => {
+        setCandidate(e.target.value);
+
+        
+        if(e.target.value == candidate1.name)
+        {
+            // can=candidate1.id;
+            setCan(candidate1.id);
+        }
+        else
+        {
+            // can=candidate2.id;
+            setCan(candidate2.id);
+        }
+        console.log("can :"+can);
+
+        // console.log("e.target.value:"+e.target.value);
+        // console.log("candidate:"+candidate);
+    };
+
+    //here we define a function onsubmit
+    const onsubmit = (e) => {
+        e.preventDefault();
+        // console.log("candidate"+candidate);
+        // console.log("candidate.id"+candidate.id);
+        if(candidate.id !== 0)
+        {
+            votecandidate(can);//Number(candidate)
+            console.log("can:"+can);
+        }
+        else
+        {
+            window.alert("Error in Voting");
+        }
+    }
+
+    return(
+        <div>
+            <table className="table">
+                <tbody>
+                    <tr><td>ID</td><td>Name</td><td>Vote Count</td></tr>
+                    <tr><td>{candidate1.id}</td><td>{candidate1.name}</td><td>{candidate1.votecount}</td></tr>
+                    <tr><td>{candidate2.id}</td><td>{candidate2.name}</td><td>{candidate2.votecount}</td></tr>
+                </tbody>
+            </table>
+            <span className="table"><b>Cast Your Vote:-</b></span>
+            <form className="table" id="select" onSubmit={onsubmit}>
+                <select onChange={onchange}>
+                    <option defaultValue value="">Select</option>
+                    <option>{candidate1.name}</option>
+                    <option>{candidate2.name}</option>
+                </select>
+                <button>Vote</button>
+                <span>You're voting for {candidate}</span>
+            </form>
+        </div>
+        
+    )
+}
+
+export default Body
